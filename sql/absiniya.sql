39a91fb9e7a10bc21d5cfa755caa51c3c9c7e034
Basliel Tegegn <basgolilo5@gmail.com>
2024-06-05 20:49:50 +0300

    update sql

--- sql/absiniya.sql
+++ sql/absiniya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 05, 2024 at 11:52 AM
+-- Generation Time: Jun 05, 2024 at 07:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -44,7 +44,31 @@ CREATE TABLE `item` (
 INSERT INTO `item` (`item_id`, `name`, `price`, `category`, `image`, `details`, `reg_date`) VALUES
 (1, 'Samsung', 120000, 'Tablet', 'https://firebasestorage.googleapis.com/v0/b/absiniyamobile.appspot.com/o/images%2Feufsqh.jpg?alt=media', '                                                                                                                                                                                       alx             \r\n                                    \r\n                                    \r\n                                    \r\n                                    ', '2024-06-04 12:22:29.471'),
 (3, 'Huawei P30', 335, 'Mobile', 'https://firebasestorage.googleapis.com/v0/b/absiniyamobile.appspot.com/o/images%2Fxuiunq.jpg?alt=media', '         huwawei', '2024-06-04 17:05:08.939'),
-(4, 'Nothing Phone (2)', 700, 'Mobile', 'https://firebasestorage.googleapis.com/v0/b/absiniyamobile.appspot.com/o/images%2Fmjydyf.jpg?alt=media', '256 GB + 12 GB Ram', '2024-06-04 18:42:51.699');
+(4, 'Nothing Phone (2)', 700, 'Mobile', 'https://firebasestorage.googleapis.com/v0/b/absiniyamobile.appspot.com/o/images%2Fmjydyf.jpg?alt=media', '256 GB + 12 GB Ram', '2024-06-04 18:42:51.699'),
+(5, 'Google - Pixel 8 Pro', 749, 'Mobile', 'https://firebasestorage.googleapis.com/v0/b/absiniyamobile.appspot.com/o/images%2Fbsvkrd.jpg?alt=media', '                                    Google - Pixel 8 Pro 128GB (Unlocked) - Obsidian', '2024-06-05 09:58:43.628');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `users_id` int(11) NOT NULL,
+  `name` text DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `phone` text DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `email_confirmed` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`users_id`, `name`, `email`, `phone`, `password`, `admin`, `email_confirmed`) VALUES
+(1, 'Basliel Tegegn', 'basgolilo5@gmail.com', '0937188813', '123123', 1, 'yes');
 
 --
 -- Indexes for dumped tables
@@ -57,6 +81,12 @@ ALTER TABLE `item`
   ADD PRIMARY KEY (`item_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -64,7 +94,13 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
